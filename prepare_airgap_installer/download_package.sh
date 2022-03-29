b369655b7a27be6d24c95640a0a68541e512192e
Ren Wang <iamwrm@gmail.com>
2022-03-29 09:38:43 +0000

    add script

--- prepare_airgap_installer/download_package.sh
+++ prepare_airgap_installer/download_package.sh
@@ -3,10 +3,16 @@ version="v1.23.4+k3s1"
 k3s_download_link=https://github.com/k3s-io/k3s/releases/download/${version}/k3s
 k3s_image_download_link=https://github.com/k3s-io/k3s/releases/download/${version}/k3s-airgap-images-amd64.tar.zst
 
-wget -c ${k3s_download_link}
+mkdir k3s_offline
+cd k3s_offline
 
-wget -c ${k3s_image_download_link}
+wget -q ${k3s_download_link}
+
+wget -q ${k3s_image_download_link}
 zstd -d k3s-airgap-images-amd64.tar.zst
 
+curl -sfL https://get.k3s.io > install.sh
+
 ls -lah
+pwd
 
