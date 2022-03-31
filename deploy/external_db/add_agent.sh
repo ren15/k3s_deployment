60ddd403ffc79f7fabdccc821655e69521f67356
Ren Wang <iamwrm@gmail.com>
2022-03-31 11:21:28 +0000

    move deploy to gitlab

--- deploy/external_db/add_agent.sh
+++
@@ -1,5 +0,0 @@
-export http_proxy=http://192.168.31.203:1087;export https_proxy=http://192.168.31.203:1087;
-
-curl -sfL https://get.k3s.io | sh -s - agent \
---token=ren_org_k3s \
---server https://192.168.31.112:6443
--- deploy/external_db/add_server.sh
+++
@@ -1,7 +0,0 @@
-export http_proxy=http://192.168.31.203:1087;export https_proxy=http://192.168.31.203:1087;
-
-DB_IP=192.168.31.188
-
-curl -sfL https://get.k3s.io | sh -s - server \
-  --token=ren_org_k3s \
-  --datastore-endpoint="http://${DB_IP}:2379"
