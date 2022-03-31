5c7f5cec297dc42dcb192430003e81b075e80c7f
Ren Wang <iamwrm@gmail.com>
2022-03-31 11:47:26 +0000

    K3S_VERSION: v1.22.7+k3s1

--- prepare_airgap_installer/download_package.sh
+++ prepare_airgap_installer/download_package.sh
@@ -1,4 +1,4 @@
-version="v1.23.4+k3s1"
+version=$1
 
 k3s_download_link=https://github.com/k3s-io/k3s/releases/download/${version}/k3s
 k3s_image_download_link=https://github.com/k3s-io/k3s/releases/download/${version}/k3s-airgap-images-amd64.tar.zst
