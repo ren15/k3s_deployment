723b62ac18c4d48031fbc83449318a1e1412b2ca
Ren Wang <iamwrm@gmail.com>
2022-10-03 03:19:27 +0000

    update

--- airgap_installer/download_package.sh
+++ airgap_installer/download_package.sh
@@ -3,8 +3,7 @@ version=$1
 k3s_download_link=https://github.com/k3s-io/k3s/releases/download/${version}/k3s
 k3s_image_download_link=https://github.com/k3s-io/k3s/releases/download/${version}/k3s-airgap-images-amd64.tar.zst
 
-mkdir k3s_offline
-cd k3s_offline
+mkdir k3s_offline && pushd k3s_offline
 
 # k3s
 wget -q ${k3s_download_link}
@@ -21,7 +20,19 @@ chmod +x install.sh
 
 ls -lah
 
-cd ..
+cat <<EOF > README.md
+export ARCH=amd64
+sudo mkdir -p /var/lib/rancher/k3s/agent/images/
+sudo cp ./k3s-airgap-images-$ARCH.tar /var/lib/rancher/k3s/agent/images/
+sudo mv k3s /usr/local/bin/k3s
+
+INSTALL_K3S_SKIP_DOWNLOAD=true ./install.sh
+INSTALL_K3S_SKIP_DOWNLOAD=true K3S_URL=https://myserver:6443 K3S_TOKEN=mynodetoken ./install.sh
+
+EOF
+
+popd
+
 tar -czvf k3s_offline_${version}.tar.gz k3s_offline
 
 ls -lah
